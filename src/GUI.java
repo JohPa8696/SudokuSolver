97e77ef54f88a595aa9a9efc3f204898a5c30b83
John Pham <dpha010@aucklanduni.ac.nz>
2016-09-01 18:15:32 +1200

    GUI

---
+++ src/GUI.java
@@ -0,0 +1,168 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.Color;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+
+import java.awt.Font;
+import java.awt.Window;
+import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.awt.event.ActionEvent;
+
+public class GUI extends JFrame implements ActionListener{
+
+	private JFrame frame;
+	private ArrayList<JTextField> list = new ArrayList<JTextField>();
+	private JFileChooser chooser;
+	private JPanel buttonPanel;
+	private String fileName;
+	
+	private JButton loadInput;
+	private JButton solve;
+	
+	
+	/**
+	 * Create the application.
+	 */
+	public GUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setBackground(Color.LIGHT_GRAY);
+		frame.setBounds(100, 100, 537, 630);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 0, 520, 530);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		int xPos=10; 
+		int yPos=10;
+		for (int i=0; i<81; i++){
+			JTextField tf= new JTextField();
+			tf.setHorizontalAlignment(JTextField.CENTER);
+			tf.setFont( new Font("SansSerif", Font.PLAIN, 18));
+			if(i%9==0 && i!=0){
+				xPos=10; 
+				if(i%27==0){
+					yPos+=60;
+				}else{
+					yPos+=55;
+				}
+			}
+			
+			tf.setColumns(10);
+			tf.setBounds(xPos,yPos,50,50);
+			list.add(tf);
+			panel.add(tf);
+			if(i%3==2){
+				xPos+=60;
+			}else{
+				xPos+=55;
+			}
+		}
+		
+		buttonPanel = new JPanel();
+		buttonPanel.setBackground(new Color(255, 255, 255));
+		buttonPanel.setBounds(0, 530, 520, 50);
+		frame.getContentPane().add(buttonPanel);
+		buttonPanel.setLayout(null);
+		
+		//load inout button
+		loadInput = new JButton("Load Input");
+		loadInput.addActionListener(this);
+		loadInput.setFont(new Font("Tahoma", Font.BOLD, 18));
+		loadInput.setBounds(0, 0, 260, 50);
+		buttonPanel.add(loadInput);
+		
+		solve = new JButton("Solve");
+		solve.setForeground(new Color(0, 0, 0));
+		solve.setFont(new Font("Tahoma", Font.BOLD, 18));
+		solve.setBounds(260, 0, 260, 50);
+		solve.addActionListener(this);
+		buttonPanel.add(solve);
+		
+		frame.setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if( e.getSource()== loadInput){
+			fileChooser();
+		}else if(e.getSource()== solve){
+			
+			
+			/*CALL THE ALGORITHM*/
+		}
+		
+	}
+	
+	
+	public void fileChooser(){
+		if (chooser == null) {
+			chooser = new JFileChooser();
+			FileNameExtensionFilter filter;
+			filter = new FileNameExtensionFilter("Input file", "txt", "dot");
+			chooser.setDialogTitle("Choose an Input File");
+			chooser.setCurrentDirectory(new java.io.File(System.getProperty("user.dir")));
+			chooser.setFileFilter((javax.swing.filechooser.FileFilter) filter);
+			chooser.setAcceptAllFileFilterUsed(false);
+			
+			if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+				fileName=chooser.getSelectedFile().toString();
+				InputProcessor ip= new InputProcessor(fileName);
+				try {
+					ip.processInput();
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				}
+				populateBoard(ip.getBoard());
+			}else{
+			}
+			chooser = null;
+		}
+	}
+	
+	
+    private void populateBoard(int[][] board){
+    	// Clear the board
+    	
+    	for(int i=0; i<list.size(); i++){
+    		JTextField tf= list.get(i);
+        	tf.setFont(new Font("SansSerif", Font.PLAIN, 18));
+        	tf.setText("");
+        	tf.setEditable(true);
+    	}
+    	// Populate the board
+    	int k =0;
+        for(int i = 0 ; i < 9; i++){
+            for( int j = 0; j < 9; j++){
+                int num=board[i][j];
+                if(num != 0){
+                	JTextField tf= list.get(k);
+                	tf.setFont(new Font("SansSerif", Font.BOLD, 18));
+                	tf.setText(num+"");
+                	tf.setEditable(false);
+                }
+                k++;
+            }
+        }
+    }
+    
+    
+	
+	
+}
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,31 @@
 
-import java.io.FileNotFoundException;
 
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+
+/**
+ * Main Class launches the launcher frame and ask user for a project directory
+ */
 public class Main {
-	public static void main(String[] args) throws FileNotFoundException{
-		String fileName="input.txt";
-		InputProcessor ip=new InputProcessor(fileName);
-		ip.processInput();
+	
+	
+	public static void main(String[] args){
 		
-	}
-}
+		try {
 
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+		
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				new GUI().setVisible(true);
+			}
+		});
+	}
+	
+
+}
